6aa4e6cf6515de8c9519f4f0883ec9e91acaf727
Heather McNeil <hd_mcneil@yahoo.com>
2023-08-01 23:27:10 -0700

    Add answers to Part 3

--- comparisons.rb
+++ comparisons.rb
@@ -96,15 +96,19 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
-
+puts loves_to_play && age < 2
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: 
+puts "The final line of code evaluated to True because the code asks if both statements are
+true. The variable loves_to_play was given a value of true and dogs are considered in the puppy phas 
+until 2 years old since both of these statements are true the full line of code it true."
+
